26b0208d6af3ecf5e6e5af008ef753afedd2ab0d
Quinth Anthony A. Razuman <31249145+quinthr@users.noreply.github.com>
2018-10-18 04:18:05 +0800

    added timeline

--- escapadeApp/models.py
+++ escapadeApp/models.py
@@ -27,6 +27,8 @@ class Write(db.Model):
     status = db.Column(db.VARCHAR)
     comment = db.Column(db.VARCHAR, nullable=True)
     region = db.relationship('Region', backref='region_Write')
+    destination = db.relationship('Destination', backref='destination_Write')
+    attraction = db.relationship('Attraction', backref='attraction_Write')
 
     def __init__(self, author_id='', author_name='', status=''):
         self.author_id = author_id
@@ -40,11 +42,52 @@ class Region(db.Model):
     content = db.Column(db.VARCHAR)
     photos = db.Column(db.LargeBinary)
     write_id = db.Column(db.Integer, db.ForeignKey('write.write_id'))
+    destination = db.relationship('Destination', backref='destination_Region')
+    attraction = db.relationship('Attraction', backref='attraction_Region')
 
     def __init__(self, name='', content='', photos='', write_id=''):
         self.name = name
         self.content = content
         self.photos = photos
+        self.write_id = write_id
+
+class Destination(db.Model):
+    __tablename__ = 'destination'
+    destination_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.VARCHAR)
+    content = db.Column(db.VARCHAR)
+    location = db.Column(db.VARCHAR)
+    photo = db.Column(db.LargeBinary)
+    write_id = db.Column(db.Integer, db.ForeignKey('write.write_id'))
+    region_id = db.Column(db.Integer, db.ForeignKey('region.region_id'))
+    attraction = db.relationship('Attraction', backref='attraction_Destination')
+
+    def __init__(self, name='', content='', location='', photo='', write_id='', region_id=''):
+        self.name = name
+        self.content = content
+        self.location = location
+        self.photo = photo
+        self.write_id = write_id
+        self.region_id = region_id
+
+class Attraction(db.Model):
+    __tablename__ = 'attraction'
+    attraction_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.VARCHAR)
+    content = db.Column(db.VARCHAR)
+    location = db.Column(db.VARCHAR)
+    photo = db.Column(db.LargeBinary)
+    region_id = db.Column(db.Integer, db.ForeignKey('region.region_id'))
+    destination_id = db.Column(db.Integer, db.ForeignKey('destination.destination_id'), nullable=True)
+    write_id = db.Column(db.Integer, db.ForeignKey('write.write_id'))
+
+    def __init__(self, name='', content='', location='', photo='', region_id='', destination_id='', write_id=''):
+        self.name = name
+        self.content = content
+        self.location = location
+        self.photo = photo
+        self.region_id = region_id
+        self.destination_id = destination_id
         self.write_id = write_id
 
 class Photo(db.Model):
